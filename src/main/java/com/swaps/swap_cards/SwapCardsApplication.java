fe0293f73691ca123cb558d3a5fb1bdc6996c7d8
prettyJaba <114904092+prettyJaba@users.noreply.github.com>
2025-02-28 09:02:44 +0400

    spring project update

---
+++ src/main/java/com/swaps/swap_cards/SwapCardsApplication.java
@@ -0,0 +1,13 @@
+package com.swaps.swap_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swaps/swap_cards/SwapCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaps.swap_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
