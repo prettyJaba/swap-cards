d051cd18787fb568d171bd7b3fdb39497397ab15
prettyJaba <114904092+prettyJaba@users.noreply.github.com>
2025-03-01 12:35:49 +0400

    LR report

--- src/main/java/com/swaps/swap_cards/SwapCardsApplication.java
+++ src/main/java/com/swaps/swap_cards/SwapCardsApplication.java
@@ -2,9 +2,7 @@ package com.swaps.swap_cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
 @SpringBootApplication
 public class SwapCardsApplication {
 
